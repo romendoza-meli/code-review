fb6245d00e54103ba978a3ca19075ef1b34f9378
Rosmar Alejandra Mendoza Canchica <romendoza@co000mt6y54042l.ml.com>
2023-10-28 20:17:55 -0500

    feature-vehicle

--- cmd/handler/vehicle.go
+++ cmd/handler/vehicle.go
@@ -1,1 +1,14 @@
 package handler
+
+func NewControllerVehicle(st service.ServiceVehicle, adapter HandlerErrorAdapter, sm mapper.StructMapper) *ControllerVehicle {
+	return &ControllerVehicle{st: st,
+		adapter: adapter,
+		sm:      sm,
+	}
+}
+
+type ControllerVehicle struct {
+	st         service.ServiceVehicle
+	adapter    mapper.StructMapper
+	errAdapter HandlerErrorAdapter
+}
